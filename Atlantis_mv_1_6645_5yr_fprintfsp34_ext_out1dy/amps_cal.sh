3a26b9ff5357324b0f011e463c12caa57baf0e27
Hem Nalini Morzaria-Luna <hmorzaria@hotmail.com>
2022-11-06 11:40:07 -0800

    first commit

---
+++ Atlantis_mv_1_6645_5yr_fprintfsp34_ext_out1dy/amps_cal.sh
@@ -0,0 +1,1 @@
+atlantisMerged -i AMPS.nc 0 -o AMPS_OUT.nc -r PugetSound_run.prm -f PugetSound_force.prm -p PugetSound_physics.prm -b AMPSbioparam_mv1_2022.prm  -h PugetSound_harvest_mfc_tuned.prm -e VMPA_setas_economics.prm -s PugetSoundAtlantisFunctionalGroups_salmon_ext.csv -q PugetSound_fisheries.csv -m PugetMigrations_mod.csv -d outputFolder 2>outamps    
